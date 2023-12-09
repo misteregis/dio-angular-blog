dd7c465d467c6599317c66b811ea1f85ef52dd57
misteregis <misteregis@gmail.com>
2023-12-09 00:29:36 -0300

    Updated dataFake

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,27 @@
 export const dataFake = [
   {
-    "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    id: 1,
+    title: "Novo Homem de Ferro em 3D",
+    description: "Marvel anuncia um novo filme do homem de ferro, confira",
+    photo: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=400&format=jpeg",
   },
   {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    id: 2,
+    title: "Nova Série anunciada no Disney+",
+    description: "Anunciada em abril deste ano, “<b>O Acólito</b>”, nova série da franquia Star Wars, chega em 2024 ao Disney+.",
+    photo:
+      "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+  },
+  {
+    id: 3,
+    title: "Novo filme do pantera negra lançado em breve",
+    description: "O filme também irá introduzir a Coração de Ferro, nova heroína vivida por Dominique Thorne, que terá sua série solo em breve.",
+    photo: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=400&format=jpeg",
+  },
+  {
+    id: 4,
+    title: "Novo filme dos X-men está em pós produção, afirma diretor",
+    description: "O filme ainda está em processo de pós-produção, que é a fase onde são colocados os efeitos visuais e sonoros, além da finalização do som e da trilha sonora. Enquanto isso, para atiçar ainda mais a curiosidade dos fãs, o estúdio colocou novas fotos da produção na web.",
+    photo: "https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg",
   }
-]
+];
